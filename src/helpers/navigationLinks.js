3a7c94d1cac3932fc69cc1ded98a3389731d2fa4
BogdaLD <liudong_xj@163.com>
2024-04-10 23:18:56 +0800

    added navigation bar

---
+++ src/helpers/navigationLinks.js
@@ -0,0 +1,22 @@
+export const navigationLinks = [
+    {
+        name: 'HOME',
+        ref: '#home'
+    },
+    {
+        name: 'ABOUT ME',
+        ref: '#about me'
+    },
+    {
+        name: 'TECHNOLOGY',
+        ref: '#technology'
+    },
+    {
+        name: 'PORTFOLIO',
+        ref: '#portfolio'
+    },
+    {
+        name: 'CONTACT',
+        ref: '#contact'
+    },
+]

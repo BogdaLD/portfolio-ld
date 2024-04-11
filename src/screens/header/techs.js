6f7a04ed2ea34f488fa56452b9e39426e37bb47c
BogdaLD <liudong_xj@163.com>
2024-04-11 19:20:50 +0800

    added techs

---
+++ src/screens/header/techs.js
@@ -0,0 +1,38 @@
+export const techs = [
+    {
+        name:'React',
+        iconnName:'react'
+    },
+    {
+        name:'Ruby',
+        iconnName:'ruby'
+    },
+    {
+        name:'Javascript',
+        iconnName:'javascript'
+    },
+    {
+        name:'CSS3',
+        iconnName:'css3'
+    },
+    {
+        name:'HTML 5',
+        iconnName:'html5'
+    },
+    {
+        name:'Docker',
+        iconnName:'docker'
+    },
+    {
+        name:'SQL lite',
+        iconnName:'sqllite'
+    },
+    {
+        name:'Bootstrap',
+        iconnName:'bootstrap'
+    },
+    {
+        name:'Github',
+        iconnName:'github'
+    },
+]
